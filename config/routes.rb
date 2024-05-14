a09e920746caa87bca5d6fe7aa277c914099bf48
adamdawkins <adam@dragondrop.uk>
2024-05-14 18:52:46 +0100

    Add notifications page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
+  resources :notifications, only: :index
   resources :change_orders
+
+
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root "change_orders#index"
+
 end
