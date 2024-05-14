b48cc42f806d397dafa926e187ecd1e960dbcefb
adamdawkins <adam@dragondrop.uk>
2024-05-14 18:44:46 +0100

    Add user role scopes

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  scope :rpc, -> { where(rpc: true) }
+  scope :finance, -> { where(finance: true) }
+  scope :pii, -> { where(pii: true) }
 end
