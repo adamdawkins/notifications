2e7ea0ea4bcbc7abf2ec83f026b003e9ea59310b
adamdawkins <adam@dragondrop.uk>
2024-05-14 23:53:41 +0100

    Style change order show, display notifications on change order

--- app/models/change_order.rb
+++ app/models/change_order.rb
@@ -1,2 +1,8 @@
 class ChangeOrder < ApplicationRecord
+  has_many :noticed_events, as: :record, dependent: :destroy, class_name: "Noticed::Event"
+  has_many :notifications, through: :noticed_events, class_name: "Noticed::Notification"
+
+  def display_id
+    "##{id.to_s.rjust(4, '0')}"
+  end
 end
