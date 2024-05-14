905649d90a3db40ddb0d6359816cf04ccf22f0c3
adamdawkins <adam@dragondrop.uk>
2024-05-14 19:15:19 +0100

    Display notifications

--- app/notifiers/change_order_notifier.rb
+++ app/notifiers/change_order_notifier.rb
@@ -3,5 +3,22 @@
 # ChangeOrderNotifier.with(record: @change_order, message: "change to project cost").deliver(User.all)
 
 class ChangeOrderNotifier < ApplicationNotifier
-     required_param :message
+  required_param :message
+ 
+  notification_methods do
+    # I18n helpers
+    def message
+      params[:message]
+    end
+ 
+    # URL helpers are accessible in notifications
+    # Don't forget to set your default_url_options so Rails knows how to generate urls
+    def url
+      change_order_path(record)
+    end
+
+    def record 
+      record
+    end
+  end
 end
