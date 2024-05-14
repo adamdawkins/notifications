8b1534a1b0be5e93fef191732febd3210f854e97
adamdawkins <adam@dragondrop.uk>
2024-05-14 18:38:49 +0100

    Remove comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  resources :comments
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
